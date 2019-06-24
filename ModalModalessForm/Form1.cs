db74bf9eee75718dc1b47de8ead03a9c174d1849
kaseul <s2017s02@e-mirim.hs.kr>
2019-06-24 09:10:41 +0900

    #2 Modal & Modaless 창 띄우기 코드 구현 by 3502 김가슬

--- ModalModalessForm/Form1.cs
+++ ModalModalessForm/Form1.cs
@@ -15,5 +15,63 @@ namespace ModalModalessForm
         {
             InitializeComponent();
         }
+
+        private void 끝내기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "종료합니다";
+            Application.ExitThread();
+        }
+
+        private void ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void ShowModal()
+        {
+            this.statusMessage.Text = "모달 창을 띄웁니다.";
+            FormModal f = new FormModal();
+            f.Title = "모달 창";
+            f.ShowDialog();
+        }
+
+        private void ModalessToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            ShowModaless();
+        }
+
+        private void ShowModaless()
+        {
+            this.statusMessage.Text = "모달리스 창을 띄웁니다.";
+            FormModaless m = new FormModaless();
+            m.Title = "모달리스 창띄우기";
+            m.Show();
+        }
+
+        private void 이프로그램은ModalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "이 프로그램은..(Modal)";
+            FormAbout f = new FormAbout();
+            f.Title = "이 프로그램은..(Modal)";
+            f.ShowDialog();
+        }
+
+        private void 이프로그램은ModalessToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.statusMessage.Text = "이 프로그램은..(Modaless)";
+            FormAbout f = new FormAbout();
+            f.Title = "이 프로그램은..(Modaless)";
+            f.Show();
+        }
+
+        private void BtmModal_Click(object sender, EventArgs e)
+        {
+            ShowModal();
+        }
+
+        private void BtnModaless_Click(object sender, EventArgs e)
+        {
+            ShowModaless();
+        }
     }
 }
